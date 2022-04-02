b51f0373815b5129ea7dd2438f7337aa66998514
Shreyas S <shreyas.bhs@gmail.com>
2022-04-02 21:56:45 +0530

    created minify

---
+++ minify/sample.js
@@ -0,0 +1,22 @@
+var a  = 1;
+var b = 2;
+//function add(a,b)
+var add = function(a,b) {
+    return a+b;
+};
+add(a,b);
+
+/*
+
+hello("world");
+
+*/
+for(var i = 0;i<10;i++){
+
+    console.log("hello");
+}
+
+var hello = function(){
+
+    console.log("world");
+}
